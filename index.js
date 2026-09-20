91547c0ff31897a589a4a1be8134a02718cf8c2e
ghost.JonnyS <jonathasoaressouza@gmail.com>
2026-09-20 09:03:12 -0300

    adiciona entrada de receitas e despesas

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
 });
 
 console.log("================================");
-console.log("    findesc    ");
+console.log("          findesc");
 console.log("================================");
 
 console.log("\n1 - Adicionar receita");
@@ -17,20 +17,27 @@ console.log("4 - Sair");
 rl.question("\nEscolha uma opção: ", (opcao) => {
 
     if (opcao === "1") {
-        console.log("adicionando receita.");
-    } 
-    else if (opcao === "2") {
-        console.log("adicionando despesa.");
-    } 
-    else if (opcao === "3") {
-        console.log("Seu saldo será mostrado aqui.");
-    } 
-    else if (opcao === "4") {
-        console.log("tchau não esquece de mim;)!");
-    } 
-    else {
-        console.log("essa opcão está errada-_-.");
-    }
+        rl.question("Digite o valor da receita: R$ ", (valor) => {
+            console.log(`Receita adicionada: R$ ${valor}`);
+            rl.close();
+        });
 
-    rl.close();
+    } else if (opcao === "2") {
+        rl.question("Digite o valor da despesa: R$ ", (valor) => {
+            console.log(`Despesa adicionada: R$ ${valor}`);
+            rl.close();
+        });
+
+    } else if (opcao === "3") {
+        console.log("Seu saldo será mostrado aqui.");
+        rl.close();
+
+    } else if (opcao === "4") {
+        console.log("Tchau, não esquece de mim ;)!");
+        rl.close();
+
+    } else {
+        console.log("Essa opção está errada -_-.");
+        rl.close();
+    }
 });
