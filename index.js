66daef120d1d3f83caebc8da032190f407bc236d
ghost.JonnyS <jonathasoaressouza@gmail.com>
2026-09-21 17:46:40 -0300

    melhorei o fluxo do menu

--- index.js
+++ index.js
@@ -5,32 +5,44 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+let receita = 0;
+let despesa = 0;
+
+
 console.log("================================");
 console.log("          findesc");
 console.log("================================");
-
 console.log("\n1 - Adicionar receita");
 console.log("2 - Adicionar despesa");
 console.log("3 - Ver saldo");
 console.log("4 - Sair");
 
+function menu() {
+
 rl.question("\nEscolha uma opção: ", (opcao) => {
 
     if (opcao === "1") {
         rl.question("Digite o valor da receita: R$ ", (valor) => {
-            console.log(`Receita adicionada: R$ ${valor}`);
-            rl.close();
+            receita += Number(valor);
+            console.log(`Receita adicionada: R$ ${receita}`);
+            menu();
         });
 
     } else if (opcao === "2") {
         rl.question("Digite o valor da despesa: R$ ", (valor) => {
-            console.log(`Despesa adicionada: R$ ${valor}`);
-            rl.close();
+            despesa += Number(valor);
+            console.log(`Despesa adicionada: R$ ${despesa}`);
+            menu();
         });
 
     } else if (opcao === "3") {
-        console.log("Seu saldo será mostrado aqui.");
-        rl.close();
+        const saldo = receita - despesa;
+
+        console.log(`Receitas: R$ ${receita.toFixed(2)}`);
+        console.log(`Despesas: R$ ${despesa.toFixed(2)}`);
+        console.log(`Saldo: R$ ${saldo.toFixed(2)}`);
+
+        menu();
 
     } else if (opcao === "4") {
         console.log("Tchau, não esquece de mim ;)!");
@@ -41,3 +53,7 @@ rl.question("\nEscolha uma opção: ", (opcao) => {
         rl.close();
     }
 });
+
+}
+
+menu(); 
